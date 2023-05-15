d2579751c94fe0a872db8ca3a7803b321629dfc4
warwickh <warwick.harris@yahoo.com.au>
2023-05-15 14:15:31 +1000

    Move ais out of lib

--- ais.py
+++ ais.py
@@ -3,6 +3,7 @@ import websockets
 import json
 from datetime import datetime, timezone
 import json
+import yaml
 
 def get_ship_data():  
     with open('ships.json') as f:
@@ -18,11 +19,20 @@ def get_ship_data():
     print(mmsi_data)
     return mmsi_data
 
-async def connect_ais_stream():
+def load_config(config_filename):
+    config = None
+    with open(config_filename, "r") as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return config
+
+async def connect_ais_stream(ais_api_key):
     mmsi_data={'257711000': 'HOEGH ASIA', '258720000': 'HOEGH BANGKOK', '257674000': 'HOEGH BEIJING', '258106000': 'HOEGH BERLIN', '257824000': 'HOEGH CHIBA', '257368000': 'HOEGH COPENHAGEN', '257869000': 'HOEGH DETROIT', '258977000': 'HOEGH JACKSONVILLE', '258975000': 'HOEGH JEDDAH', '258981000': 'HOEGH LONDON', '259709000': 'HOEGH MANILA', '257560000': 'HOEGH NEW YORK', '259739000': 'HOEGH OSLO', '257496000': 'HOEGH SEOUL', '258758000': 'HOEGH SHANGHAI', '257366000': 'HOEGH ST.PETERSBURG', '257864000': 'HOEGH TARGET', '257408000': 'HOEGH TOKYO', '258628000': 'HOEGH TRACER', '258882000': 'HOEGH TRADER', '257712000': 'HOEGH TRANSPORTER', '258872000': 'HOEGH TRAPPER', '259075000': 'HOEGH TRAVELLER', '257713000': 'HOEGH TRIDENT', '258389000': 'HOEGH TRIGGER', '257714000': 'HOEGH TROOPER','259274000': 'HOEGH TROTTER', '258774000': 'HOEGH TROVE'}
      
     async with websockets.connect("wss://stream.aisstream.io/v0/stream") as websocket:
-        subscribe_message = {"APIKey": "7d11430df0181f6f32e3f8ce9ec462815cc3e563", "BoundingBoxes": [[[-180, -90], [180, 90]]]}
+        subscribe_message = {"APIKey": ais_api_key, "BoundingBoxes": [[[-180, -90], [180, 90]]]}
 
         subscribe_message_json = json.dumps(subscribe_message)
         await websocket.send(subscribe_message_json)
@@ -42,4 +52,7 @@ async def connect_ais_stream():
 
 if __name__ == "__main__":
     #mmsi_data = get_ship_data()
-    asyncio.run(connect_ais_stream())
+    config_filename = 'config.yml'
+    config = load_config(config_filename)
+    ais_api_key = config["ais_api_key"]
+    asyncio.run(connect_ais_stream(ais_api_key))
