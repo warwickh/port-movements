398976094d2337044751678d1debb0dd628d3cb9
warwickh <warwick.harris@yahoo.com.au>
2023-05-16 16:17:34 +1000

    Add speed/direction

--- lib/aissession.py
+++ lib/aissession.py
@@ -4,7 +4,9 @@ import json
 from datetime import datetime, timezone
 import json
 import yaml
-
+from geopy import distance
+import math, numpy as np
+   
 class AisSession:
     def __init__(self,
                  ais_api_key='',
@@ -31,18 +33,43 @@ class AisSession:
             with open(self.ship_data_file, 'w') as f:
                 f.write(json.dumps(self.ship_data, indent=4))
 
+    def get_bearing(self, lat1, lon1, lat2, lon2):
+        dLon = lon2 - lon1;
+        y = math.sin(dLon) * math.cos(lat2);
+        x = math.cos(lat1)*math.sin(lat2) - math.sin(lat1)*math.cos(lat2)*math.cos(dLon);
+        brng = np.rad2deg(math.atan2(y, x));
+        if brng < 0: brng+= 360
+        return brng
+
     def update_location(self, mmsi, lat, lon):
         ship = self.ship_data[str(mmsi)]
         d = datetime.now()
-        unixtime = datetime.timestamp(d)*1000
+        unixtime = int(round(datetime.timestamp(d)*1000))
+        last_time = float(ship["UPDATED"])
+        last_lat = float(ship["LAT"])
+        last_lon = float(ship["LON"])
+        trav_dist = distance.distance((lat, lon), (last_lat, last_lon)).km
+        millis = int(round(unixtime))-int(round(last_time))
+        trav_time = hours=(millis/(1000*60*60))%24
+        trav_speed = trav_dist/trav_time
+        trav_dir = self.get_bearing(lat, lon, last_lat, last_lon)
+        #print("Dist: %s"%trav_dist)
+        #print("Time: %s"%(trav_time))
+        #print("Speed %s"%trav_speed)#/1.852
+        print("Speed %s knts"%str(trav_speed/1.852))
+        print("Dir: %s"%trav_dir)
         ship["UPDATED"] = str(unixtime)
         ship["LAT"] = str(lat)
         ship["LON"] = str(lon)
+        ship["SPEED"] = str(trav_speed/1.852)
+        ship["DIR"] = str(trav_dir)
         self.ship_data[str(mmsi)] = ship
         self.save_ships()
 
     def get_distance(self, lat, lon, port):
         port_data={'AUPKL': {'Latitude': '-34.46346', 'Longitude': '150.901482691176'}, 'AUMEL': {'Latitude': '-37.81325655', 'Longitude': '144.924152576608'}, 'AUBNE': {'Latitude': '-27.385741', 'Longitude': '153.17374430786'}, 'NZAKL': {'Latitude': '-36.9323169', 'Longitude': '174.784926235455'}, 'AUFRE': {'Latitude': '-32.0307289', 'Longitude': '115.7480727'}  }
+        
+        
 
     async def connect_ais_stream(self):
         async with websockets.connect("wss://stream.aisstream.io/v0/stream") as websocket:
